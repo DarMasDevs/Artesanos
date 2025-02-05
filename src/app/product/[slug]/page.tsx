30f48b08fb4fa386842b69825b72f47ec44208f2
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-02-04 21:01:55 -0300

    ajustes en page de detalle

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import { Products } from '@/types/types';
 import { data } from '../../../../public/data';
+import { Products } from '@/types/types';
 import Details from '@/components/Details/Details';
 
 interface ProductDetailProps {
@@ -9,24 +9,27 @@ interface ProductDetailProps {
 }
 
 const ProductDetail = ({ params }: ProductDetailProps) => {
-  //obtencion de slug  
-  const { slug } = params;
+  const slug = params.slug;
   const [id] = slug.split('-');
-
-  //obtencion de producto
-  const product = data.products.find((product: Products) => product._id === id);
+  const product = data.products.find((product: Products) => product._id === id) || null;
 
   if (!product) {
     return <div>Producto no encontrado</div>;
   }
 
-  
-
   return (
     <div className="container mx-auto px-4 py-8">
-    <Details product={product} />
+      <Details product={product} />
     </div>
   );
 };
+
+export async function generateStaticParams() {
+  const paths = data.products.map((product) => ({
+    slug: `${product._id}-${product.title.replace(/\s+/g, '-').toLowerCase()}`,
+  }));
+
+  return paths;
+}
 
 export default ProductDetail;
