dda55b701cb5a2fdfd916749fe68846ac95faf10
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-02-13 16:36:50 -0300

    cierre de sesion agregado con toast

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,49 +1,90 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import { ShoppingBagIcon } from "lucide-react";
+import { Settings, ShoppingBagIcon, User } from "lucide-react";
 import { navlinks } from "./NavLinks";
 import { routes } from "@/config/routes";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/types/types";
 import Image from "next/image";
+import { logoutUser } from "@/redux/features/userSlice";
+import { toast, ToastContainer } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 interface NavBarProps {
   open: boolean;
 }
 
 const NavBar = ({ open }: NavBarProps) => {
+  const dispatch = useDispatch();
   const cartItems = useSelector(
     (state: RootState) => state.cartReducer.cartItems,
   );
   const user = useSelector((state: RootState) => state.userReducer.user);
-
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const profileMenuRef = useRef<HTMLDivElement>(null);
   const [cartItemsCount, setCartItemsCount] = useState(0);
-
+  const router = useRouter();
   useEffect(() => {
-    const count = cartItems.reduce((total, item) => total + item.quantity, 0);
-    if (count !== cartItemsCount) {
-      setCartItemsCount(count);
-    }
-  }, [cartItems, cartItemsCount]);
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        profileMenuRef.current &&
+        !profileMenuRef.current.contains(event.target as Node)
+      ) {
+        setIsProfileMenuOpen(false);
+      }
+    };
 
-  const userLink = user ? routes.profile : routes.login;
-  const userContent = user ? (
-    <div className="flex">
-      <div className="relative">
-        <Image
-          width={40}
-          height={40}
-          src={user.image || "https://via.placeholder.com/40"}
-          alt="avatar"
-          className="h-10 w-10 rounded-full"
-        />
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  
+  useEffect(() => {
+    setCartItemsCount(
+      cartItems.reduce((total, item) => total + item.quantity, 0),
+    );
+  }, [cartItems]);
+
+  const logout = () => {
+    dispatch(logoutUser());
+    router.push(routes.login);
+    toast.success("Sesión cerrada");
+    toast.dismiss();
+    setIsProfileMenuOpen(false);
+  };
+
+  
+  const handleLogout = () => {
+    const LogoutConfirmation = () => (
+      <div className="bg-dark-brown p-4 text-white">
+        <p>¿Estás seguro de que quieres cerrar sesión?</p>
+        <div className="mt-4 flex justify-end gap-2">
+          <button
+            onClick={logout}
+            className="bg-red-500 hover:bg-red-600 rounded-md px-4 py-2 "
+          >
+            Sí
+          </button>
+          <button
+            onClick={() => toast.dismiss()}
+            className="bg-gray-100 hover:bg-gray-200 rounded-md px-4 py-2 "
+          >
+            No
+          </button>
+        </div>
       </div>
-    </div>
-  ) : (
-    <button className="flex items-center gap-2 px-3 py-2 text-xs underline-offset-1 hover:text-light-brown hover:underline">
-      Iniciar sesión
-    </button>
-  );
+    );
+
+    toast.info(<LogoutConfirmation />, {
+      position: "top-center",
+      autoClose: false,
+      closeOnClick: false,
+      draggable: false,
+      closeButton: false,
+      toastId: "logout-confirmation",
+      theme: "dark",
+    });
+  };
 
   return (
     <div className="flex items-center justify-between gap-5">
@@ -85,8 +126,53 @@ const NavBar = ({ open }: NavBarProps) => {
             <span className="pl-1 text-brown">{cartItemsCount}</span>
           </div>
         </Link>
+        {user ? (
+          <div className="relative" ref={profileMenuRef}>
+            <button
+              onClick={() => setIsProfileMenuOpen(!isProfileMenuOpen)}
+              className="flex items-center"
+            >
+              <Image
+                width={40}
+                height={40}
+                src={user.image}
+                alt="User avatar"
+                className="h-10 w-10 rounded-full border-2 border-brown"
+              />
+            </button>
+
+            {isProfileMenuOpen && (
+              <div className="absolute right-0 top-12 z-50 w-48 rounded-md bg-white shadow-lg">
+                <Link
+                  href={routes.profile}
+                  onClick={() => setIsProfileMenuOpen(false)}
+                  className="text-gray-700 hover:bg-gray-100 block px-4 py-2"
+                >
+                  <div className="flex items-center">
+                    <User className="mr-2 h-5 w-5" />
+                    Perfil
+                  </div>
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className="text-gray-700 hover:bg-gray-100 w-full px-4 py-2 text-left"
+                >
+                  <div className="flex items-center">
+                    <Settings className="mr-2 h-5 w-5" />
+                    Cerrar sesión
+                  </div>
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <Link href={routes.login}>
+            <button className="flex items-center gap-2 px-3 py-2 text-xs underline-offset-1 hover:text-light-brown hover:underline">
+              Iniciar sesión
+            </button>
+          </Link>
+        )}
       </div>
-      <Link href={userLink}>{userContent}</Link>
 
       <div className="md:hidden">
         {open ? (
@@ -104,6 +190,17 @@ const NavBar = ({ open }: NavBarProps) => {
           </dialog>
         ) : null}
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
