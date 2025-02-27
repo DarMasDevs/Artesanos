b7d0e2d324529ec8f50b7abf8caa314424596bfe
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-02-27 19:49:40 -0300

    Refactor NavBar component: improve styling and add hover interactions

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -39,10 +39,10 @@ const NavBar = ({ open }: NavBarProps) => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getlogindata());
   }, [dispatch]);
-  
+
   useEffect(() => {
     const count = cartItems.reduce((total, item) => total + item.quantity, 0);
     if (count !== cartItemsCount) {
@@ -57,7 +57,6 @@ const NavBar = ({ open }: NavBarProps) => {
     setIsProfileMenuOpen(false);
   };
 
-  
   const handleLogout = () => {
     const LogoutConfirmation = () => (
       <div className="bg-dark-brown p-4 text-white">
@@ -65,16 +64,16 @@ const NavBar = ({ open }: NavBarProps) => {
         <div className="mt-4 flex justify-end gap-2">
           <button
             onClick={() => {
-              toast.dismiss()
+              toast.dismiss();
               logout();
             }}
-            className="bg-red-500 hover:bg-red-600 rounded-md px-4 py-2 "
+            className="bg-red-500 hover:bg-red-600 rounded-md px-4 py-2"
           >
             Sí
           </button>
           <button
             onClick={() => toast.dismiss()}
-            className="bg-gray-100 hover:bg-gray-200 rounded-md px-4 py-2 "
+            className="bg-gray-100 hover:bg-gray-200 rounded-md px-4 py-2"
           >
             No
           </button>
@@ -94,7 +93,7 @@ const NavBar = ({ open }: NavBarProps) => {
   };
 
   return (
-    <div className="flex items-center justify-between gap-5 ">
+    <div className="flex items-center justify-between gap-5">
       <div className="hidden md:flex lg:flex">
         <div className="flex items-center space-x-3">
           {navlinks.map((link, index) => (
@@ -106,7 +105,7 @@ const NavBar = ({ open }: NavBarProps) => {
                 {link.nombre}
               </Link>
               {link.sublinks && (
-                <div className="absolute left-0 mt-2 w-48 rounded-md bg-white opacity-0 shadow-lg transition-opacity duration-300 group-hover:opacity-100">
+                <div className="pointer-events-none absolute left-0 mt-2 w-48 rounded-md bg-white opacity-0 shadow-lg transition-opacity duration-300 group-hover:pointer-events-auto group-hover:opacity-100">
                   {link.sublinks.map((sublink, subIndex) => (
                     <Link
                       key={subIndex}
@@ -147,35 +146,36 @@ const NavBar = ({ open }: NavBarProps) => {
                 className="h-10 w-10 rounded-full border-2 border-brown"
               />
             </button>
-          <AnimatePresence>
-            {isProfileMenuOpen && (
-              <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3 }}
-              className="absolute right-0 top-12 z-50 w-48 rounded-md bg-white shadow-lg">
-                <Link
-                  href={routes.profile}
-                  onClick={() => setIsProfileMenuOpen(false)}
-                  className="text-gray-700 hover:bg-gray-100 block px-4 py-2"
+            <AnimatePresence>
+              {isProfileMenuOpen && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="absolute right-0 top-12 z-50 w-48 rounded-md bg-white shadow-lg"
                 >
-                  <div className="flex items-center">
-                    <User className="mr-2 h-5 w-5" />
-                    Perfil
-                  </div>
-                </Link>
-                <button
-                  onClick={handleLogout}
-                  className="text-gray-700 hover:bg-gray-100 w-full px-4 py-2 text-left"
-                >
-                  <div className="flex items-center">
-                    <Settings className="mr-2 h-5 w-5" />
-                    Cerrar sesión
-                  </div>
-                </button>
-              </motion.div >
-            )}
+                  <Link
+                    href={routes.profile}
+                    onClick={() => setIsProfileMenuOpen(false)}
+                    className="text-gray-700 hover:bg-gray-100 block px-4 py-2"
+                  >
+                    <div className="flex items-center">
+                      <User className="mr-2 h-5 w-5" />
+                      Perfil
+                    </div>
+                  </Link>
+                  <button
+                    onClick={handleLogout}
+                    className="text-gray-700 hover:bg-gray-100 w-full px-4 py-2 text-left"
+                  >
+                    <div className="flex items-center">
+                      <Settings className="mr-2 h-5 w-5" />
+                      Cerrar sesión
+                    </div>
+                  </button>
+                </motion.div>
+              )}
             </AnimatePresence>
           </div>
         ) : (
